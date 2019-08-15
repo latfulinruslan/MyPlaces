b21fc169046ff649d9e430e65552c13d2ade1466
Ruslan Latfulin <latfulinr@gmail.com>
2019-08-15 17:16:18 +0300

    Design for NewPlaceViewController

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Ruslan Latfulin on 8/15/19.
+//  Copyright © 2019 Ruslan Latfulin. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView() 
+    }
+    
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
